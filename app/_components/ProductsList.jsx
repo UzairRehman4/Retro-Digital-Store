ee7de12474e98cfb304fc9ea7d0f836f6f7b1863
Uzair.dev <rehmanuzair43@gmail.com>
2024-11-24 21:46:30 +0500

    clerk authentication added ✔

--- app/_components/ProductsList.jsx
+++ app/_components/ProductsList.jsx
@@ -1,30 +1,32 @@
-"use client"
-import React, { useEffect, useState } from 'react'
+"use client";
+import React, { useEffect, useState } from 'react';
 import { Button } from '@/components/ui/button';
 import Products from '../_mockData/Products';
 import ProductCartItem from './ProductCartItem';
+
 const ProductsList = () => {
-    const [ProductsList, setProductsList] = useState([]);
+    const [productsList, setProductsList] = useState([]);
 
     useEffect(() => {
-        setProductsList(Products)
+        // Setting the products list when the component mounts
+        setProductsList(Products);
     }, []);
+
     return (
         <div>
             <h2 className='font-bold text-xl flex justify-between items-center'>
                 Featured <span><Button>View All</Button></span>
             </h2>
 
-            <div className='grid grid-col-2 lg:grid-cols-3 xl:grid:cols-3 gap-5 mt-8'>
+            <div className='grid grid-cols-2 lg:grid-cols-3 xl:grid-cols-3 gap-5 mt-8'>
                 {
-                    ProductsList.map((product, index) => (
-                        <ProductCartItem product={product} key={index} />
-                    )
-                    )
+                    productsList.map((product, index) => (
+                        <ProductCartItem product={product} key={product.id || index} />
+                    ))
                 }
             </div>
         </div>
-    )
-}
+    );
+};
 
-export default ProductsList
+export default ProductsList;
